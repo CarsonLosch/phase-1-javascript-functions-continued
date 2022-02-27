42de1065fc8f50beaf91d4e64f40ba4670f627b7
CarsonLosch <95552371+CarsonLosch@users.noreply.github.com>
2022-02-27 11:48:18 -0800

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+const mondayWork = function(action = "go to the office" ) {
+    return (`This Monday, I will ${action}.`);
+}
+mondayWork();
+
+function wrapAdjective(flare = "*") {
+    return function (attribute = "special") {
+        return `You are ${flare}${attribute}${flare}!`;
+    };
+}
+wrapAdjective("%")("a dedicated programmer");
